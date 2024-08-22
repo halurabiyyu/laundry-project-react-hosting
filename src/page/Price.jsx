c33247bfd4868863b114390adc6f8164c252cd6c
halurabiyyu <halurabiyyu@gmail.com>
2024-08-22 22:41:08 +0700

    feat : add responsive mobile

--- src/page/Price.jsx
+++ src/page/Price.jsx
@@ -12,21 +12,21 @@ export default function Price() {
     <>
       <div className="container h-screen flex justify-center m-auto" >
         <div
-          className="w-[80%] bg-white container flex justify-center content-center m-auto mt-0"
+          className="w-[80%] bg-white container flex flex-col-reverse gap-1 lg:flex-row justify-center content-center m-auto mt-0"
           data-aos="fade-up"
           data-aos-duration="700"
           id="price"
         >
           <div className="container flex-1" >
-            <div className="container flex justify-end ">
-              <h1 className="bg-[#FFE000] p-1 rounded me-5 text-white inline font-bold right-0">
+            <div className="container flex lg:justify-end ">
+              <h1 className="bg-[#FFE000] p-1 rounded me-5 text-white inline font-bold lg:right-0">
                 Price List
               </h1>
             </div>
-            <h1 className="font-bold text-7xl text-right me-5">
+            <h1 className="font-bold lg:text-7xl text-3xl lg:text-right me-5">
               Affordable Care, Premium Clean.
             </h1>
-            <div className="container flex flex-row justify-end gap-1">
+            <div className="container flex flex-col lg:flex-row justify-end gap-1">
               <div className="mt-4 bg-white drop-shadow-lg flex-grow h-[215px] hover:outline p-3 me-5 font-bold text-center">
                 <span className="rounded">Basic</span>
                 <h1 className="text-4xl mt-2">$7</h1>
