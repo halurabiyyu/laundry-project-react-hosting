8c4fec85ac55fcbbbbc24c230fc5491d8183fde0
halurabiyyu <halurabiyyu@gmail.com>
2024-08-23 19:28:36 +0700

    feat : change from hardcode to json format for price list

--- src/page/Price.jsx
+++ src/page/Price.jsx
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from "react";
 import WashMachinePicts from "../assets/many-machine.jpg";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+// import BoxPriceList from "../components/BoxPriceList";
+// import Data from "../data_text/priceList.json"
 
 export default function Price() {
   useEffect(() => {
     AOS.init();
   }, []);
 
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/data_text/priceList.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching the JSON: ", error));
+  }, []);
+
   return (
     <>
-      <div className="container h-screen flex justify-center m-auto" >
+      <div className="container h-screen flex justify-center m-auto">
         <div
           className="w-[80%] bg-white container flex flex-col-reverse gap-1 lg:flex-row justify-center content-center m-auto mt-0"
           data-aos="fade-up"
           data-aos-duration="700"
           id="price"
         >
-          <div className="container flex-1" >
+          <div className="container flex-1">
             <div className="container flex lg:justify-end ">
               <h1 className="bg-[#FFE000] p-1 rounded me-5 text-white inline font-bold lg:right-0">
                 Price List
@@ -27,39 +43,26 @@ export default function Price() {
               Affordable Care, Premium Clean.
             </h1>
             <div className="container flex flex-col lg:flex-row justify-end gap-1">
-              <div className="mt-4 bg-white drop-shadow-lg flex-grow h-[215px] hover:outline p-3 me-5 font-bold text-center">
-                <span className="rounded">Basic</span>
-                <h1 className="text-4xl mt-2">$7</h1>
-                <div className="text-left font-normal pt-7 ps-2 flex justify-center">
-                  <ul className="ps-1 list-disc list-outside">
-                    <li>Max 7Kg</li>
-                    <li>Wash & Fold</li>
-                    <li>Ironing</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mt-4 bg-white drop-shadow-lg flex-grow h-[215px] hover:outline p-3 me-5 font-bold text-center">
-                <span className="rounded">Premium</span>
-                <h1 className="text-4xl mt-2">$12</h1>
-                <div className="text-left font-normal pt-7 ps-2 flex justify-center">
-                  <ul className="ps-1 list-disc list-outside">
-                    <li>Max 9Kg</li>
-                    <li>Dry Cleaning</li>
-                    <li>Delicate Fabrics</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mt-4 bg-white drop-shadow-lg flex-grow h-[215px] hover:outline p-3 me-5 font-bold text-center">
-                <span className="rounded">Express</span>
-                <h1 className="text-4xl mt-2">$15</h1>
-                <div className="text-left font-normal pt-7 ps-2 flex justify-center">
-                  <ul className="ps-1 list-disc list-outside">
-                    <li>Max 7Kg</li>
-                    <li>Same-Day Service</li>
-                    <li>24-Hour Turnaround</li>
-                  </ul>
-                </div>
-              </div>
+              {data && data.price_list ? (
+                data.price_list.map((priceItem) => (
+                  <div
+                    key={priceItem.name_price_list}
+                    className="mt-4 bg-white drop-shadow-lg flex-grow h-[215px] hover:outline p-3 me-5 font-bold text-center"
+                  >
+                    <span className="rounded">{priceItem.name_price_list}</span>
+                    <h1 className="text-4xl mt-2">${priceItem.price}</h1>
+                    <div className="text-left font-normal pt-7 ps-2 flex justify-center">
+                      <ul className="ps-1 list-disc list-outside">
+                        {priceItem.benefit.map((benefitItem) => (
+                          <li key={benefitItem.id}>{benefitItem.value}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>Loading...</p> // Or any other loading indicator
+              )}
             </div>
           </div>
           <div className="flex-1 container h-auto rounded">
