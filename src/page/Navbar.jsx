a81dbdf69eeb63e9735dc8e6313789d7a86284da
halurabiyyu <halurabiyyu@gmail.com>
2024-08-26 20:53:51 +0700

    feat : responsive navbar mobile

--- src/page/Navbar.jsx
+++ src/page/Navbar.jsx
@@ -1,25 +1,77 @@
 import React, { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
-    const navigations = [
-        { name: "Home", href:"#home"},
-        { name: "About Us", href:"#about"},
-        { name: "Price List", href:"#price"},
-    ];
+  const navigations = [
+    { name: "Home", href: "#home" },
+    { name: "About Us", href: "#about" },
+    { name: "Price List", href: "#price" },
+  ];
 
-    const [openMobile, setMobile] = useState(false)
+  const [openMobile, setMobileOpen] = useState(false);
 
-    return (
-        <>
-            <nav className="bg-white h-16 sticky top-0 font-poppins shadow-sm z-20">
-                <ul className="h-full flex justify-center content-center gap-4 text-[#176B87]">
-                    {navigations.map((item)=>(
-                        <li key={item.name} className="content-center text-lg">
-                            <a href={item.href} className="hover:font-bold hover:text-white hover:bg-[#FFE000] p-2 rounded">{item.name}</a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className=" bg-white h-16 sticky top-0 font-poppins shadow-sm z-20">
+        <ul className="hidden lg:flex h-full justify-center content-center gap-4 text-[#176B87]">
+          {navigations.map((item) => (
+            <li key={item.name} className="content-center text-lg">
+              <a
+                href={item.href}
+                className="hover:font-bold hover:text-white hover:bg-[#FFE000] p-2 rounded"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex lg:hidden justify-end pt-5 pe-5">
+          <button
+            type="button"
+            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileOpen(true)}
+          >
+            <span className="sr-only">Open Main Menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+      </nav>
+      {/* modal main menu mobile  */}
+      <Dialog className="lg:hidden" open={openMobile} onClose={setMobileOpen}>
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Halur Muhammad Abiyyu</span>
+              {/* <img className="h-8 w-auto" src={Logo} alt="" /> */}
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6 ">
+                {navigations.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 bg-white hover:bg-indigo-600"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </>
+  );
 }
